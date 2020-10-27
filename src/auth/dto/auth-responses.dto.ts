9d18073dda902d7fbb5bb89c4254eaafb7ad6975
unknown <afaizi1999@gmail.com>
2020-10-27 16:54:35 +0700

    Combine responses

--- src/auth/dto/auth-responses.dto.ts
+++ src/auth/dto/auth-responses.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Status, BaseResponse } from 'src/utils/responseJson';
-import { User } from '../user.entity';
+import { User } from '../../user/user.entity';
 
 export class ISignInResponseData {
   token: string;
@@ -15,4 +15,19 @@ export class ISignInResponse implements BaseResponse<ISignInResponseData> {
   status: Status;
   message: string;
   data: ISignInResponseData;
+}
+
+export class ISignUpResponseData {
+  token: string;
+  user: User;
+}
+
+export class ISignUpResponse implements BaseResponse<ISignUpResponseData> {
+  @ApiProperty({
+    enum: Status,
+    enumName: 'Status',
+  })
+  status: Status;
+  message: string;
+  data: ISignUpResponseData;
 }
