c55e4c7e5a54fe4e2a8520f95a25ce3fec9d4f81
unknown <afaizi1999@gmail.com>
2020-11-06 15:53:48 +0700

    Create some modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as typeORMConfig from '../typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { PaymentModule } from './payment/payment.module';
+import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule, UserModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule, UserModule, ProductModule, CartModule, PaymentModule, WishlistModule],
 })
 export class AppModule {}
